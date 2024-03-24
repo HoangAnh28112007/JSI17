0e80d0f0e98a4aede719ca1dc1b846100e728949
HoangAnh28112007 <phananhhoang28111997@gmail.com>
2024-03-24 09:57:22 +0700

    bannhabancua

--- spck/A ĐĂNG KÍ, ĐĂNG NHẬP/DK.js
+++ spck/A ĐĂNG KÍ, ĐĂNG NHẬP/DK.js
@@ -1,1 +1,11 @@
+const dang_ky = () =>{
+    const email = document.querySelector(".email").value
+    const pass = document.querySelector(".password").value
+    const data = {
+        name: email,
+        password: pass
+    };
+}
 
+window.location.href = "./DN.html"
+    alert("! Sign up successed !")
